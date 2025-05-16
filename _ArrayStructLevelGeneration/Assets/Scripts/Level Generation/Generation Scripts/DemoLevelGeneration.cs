ebafb74205241bfa9b6915e0d004a15ac4c214a7
Fhlic <adefra5@lsu.edu>
2025-05-16 17:22:13 -0500

    [feat] added in the scene and level generation files and a new scene for loading the different levels

---
+++ _ArrayStructLevelGeneration/Assets/Scripts/Level Generation/Generation Scripts/DemoLevelGeneration.cs
@@ -0,0 +1,55 @@
+using System.Linq;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class DemoLevelGeneration : MonoBehaviour
+{
+    public Walker1x1ChunkLevel level;
+
+    public Tilemap floorTilemap;
+    public Tilemap wallTilemap;
+    public Tile wall;
+    public Tile floor;
+
+    public int tileCount = default;
+
+    public void GenerateLevel()
+    {
+        level = new Walker1x1ChunkLevel();
+
+        floorTilemap.ClearAllTiles();
+        wallTilemap.ClearAllTiles();
+        tileCount = default;
+
+        foreach (Walker4rmChunk chunk in level.chunkList)
+        {
+            foreach (WalkerRoom1x1 room in chunk.roomList)
+            {
+                CycleRoom(room);
+            }
+        }
+    }
+    private void CycleRoom(WalkerRoom1x1 room)
+    {
+        Vector3Int initialTilePlacer = new Vector3Int((int)room.roomPos.x, (int)room.roomPos.y, 0);
+
+        for (int i = 0; i < room.roomGrid.GetLength(0); i++)
+        {
+            for (int j = 0; j < room.roomGrid.GetLength(1); j++)
+            {
+                Vector3Int tilePlacer = new Vector3Int(initialTilePlacer.x + i, initialTilePlacer.y + j, 0);
+
+                if (room.roomGrid[i, j] == AbstractRoom.Grid.FLOOR)
+                {
+                    floorTilemap.SetTile(tilePlacer, floor);
+                    tileCount++;
+                }
+                if (room.roomGrid[i, j] == AbstractRoom.Grid.WALL)
+                {
+                    wallTilemap.SetTile(tilePlacer, wall);
+                }
+            }
+        }
+    }
+}
